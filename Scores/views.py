6c06fb612c4c870ae82a7df0e174a73e58b87425
b-isfor-basic <93852853+b-isfor-basic@users.noreply.github.com>
2022-06-04 10:50:23 -0400

    Modify frontend

--- Scores/views.py
+++ Scores/views.py
@@ -4,7 +4,7 @@ from django.views.generic import CreateView
 from Schedule.models import Match
 from .models import PlayerScore, TeamScore
 
-# Create your views here.
+
 def ScoresheetCreateView(request, pk):
     match_result = get_object_or_404(Match, pk=id)
     template_name = "add_scoresheet.html"
