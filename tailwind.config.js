e75787459eb228e0cc64e1e3ae41d9f34eac2d81
b-isfor-basic <93852853+b-isfor-basic@users.noreply.github.com>
2022-07-31 08:14:49 -0400

    change css to match new scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     './node_modules/flowbite/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': {
+          '50': '#f2f1fe',
+          '100': '#ddd9fc',
+          '200': '#b5b3f9',
+          '300': '#887ef7',
+          '400': '#503df5',
+          '500': '#2609e1',
+          '600': '#2506b2',
+          '700': '#180391',
+          '800': '#0f016a',
+          '900': '#05002e',
+        },      
+      },
+    },
   },
   plugins: [
     require('flowbite/plugin')
