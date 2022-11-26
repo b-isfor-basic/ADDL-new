56348004df510c0fe5a3d86e97d16665e2316abc
b-isfor-basic <overt_link0m@icloud.com>
2022-11-26 18:04:39 -0500

    prep deployment

--- ADDL/settings.py
+++ ADDL/settings.py
@@ -18,7 +18,7 @@ from django.test.runner import DiscoverRunner
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-IS_HEROKU = "dyno" in os.environ
+IS_HEROKU = "DYNO" in os.environ
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -30,7 +30,7 @@ if 'SECRET_KEY' in os.environ:
 if IS_HEROKU:
     ALLOWED_HOSTS = ["*"]
 else:
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ["localhost", ""]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if not IS_HEROKU:
