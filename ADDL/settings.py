9d93c154d0e8ebe81b76ae1cd47e63b24213336d
b-isfor-basic <overt_link0m@icloud.com>
2022-11-27 16:28:18 -0500

    fix schedule filters

--- ADDL/settings.py
+++ ADDL/settings.py
@@ -30,7 +30,8 @@ if 'SECRET_KEY' in os.environ:
 if IS_HEROKU:
     ALLOWED_HOSTS = ["*"]
 else:
-    ALLOWED_HOSTS = [""]
+    ALLOWED_HOSTS = ["0.0.0.0"]
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if not IS_HEROKU:
