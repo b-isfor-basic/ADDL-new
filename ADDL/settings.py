7ab5a1493e42de0f6f02db2efe2bffb2f75cb5df
b-isfor-basic <overt_link0m@icloud.com>
2022-12-13 10:08:13 -0500

    add sentry

--- ADDL/settings.py
+++ ADDL/settings.py
@@ -215,3 +215,22 @@ else:
 # Login Redirects
 
 LOGIN_REDIRECT_URL = "profile"
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://029ebd70cdbb4da78e1db1f5712cb6c1@o4504322212233216.ingest.sentry.io/4504322216951808",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
