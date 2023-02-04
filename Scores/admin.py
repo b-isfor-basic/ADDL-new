6274a950be3dfa7514d26e8ca71093d290c663e6
b-isfor-basic <overt_link0m@icloud.com>
2023-02-04 15:35:26 -0500

    hotfix score summary model admin

--- Scores/admin.py
+++ Scores/admin.py
@@ -6,9 +6,7 @@ from .models import ScoreDetail, ScoreSummary, TeamScoreSummary
 @admin.register(ScoreSummary)
 class ScoreSummaryAdmin(admin.ModelAdmin):
     list_filter = ["match__week__division", "match__week__week_number"]
-    list_display = ["team", "player", "total_points", "singles_weekly_ppd", "total_stars", "avg_stars_per_game", "total_perfects", "win_pct"]
-
-
+    list_display = ["team", "player", "total_points", "singles_weekly_ppd", "total_stars", "total_perfects"]
 
 
 @admin.register(ScoreDetail)
