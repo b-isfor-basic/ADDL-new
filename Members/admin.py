126d5adb4200f55f09188d64890948399b9fd9f0
b-isfor-basic <overt_link0m@icloud.com>
2023-05-15 23:26:36 -0400

    Update team display name in admin

--- Members/admin.py
+++ Members/admin.py
@@ -11,6 +11,11 @@ class PlayerAdmin(UserAdmin):
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
+    list_select_related = ("season", "division")
     list_display = ("season", "division", "name")
     list_filter = ("season", "division")
     search_fields = ["name"]
+
+    def name(self, obj):
+        plyrs = obj.players.all()
+        return plyrs[0].last_name + "/" + plyrs[1].last_name
