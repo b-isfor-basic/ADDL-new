c3e77fe7d3ed14fab79310ac4ecfb2881e616d5d
b-isfor-basic <overt_link0m@icloud.com>
2023-05-21 06:18:22 -0400

    Rename and trim schedule models, update admin

--- Scores/admin.py
+++ Scores/admin.py
@@ -5,17 +5,52 @@ from .models import ScoreDetail, ScoreSummary, TeamScoreSummary
 
 @admin.register(ScoreSummary)
 class ScoreSummaryAdmin(admin.ModelAdmin):
-    list_filter = ["match__week__division", "match__week__week_number"]
-    list_display = ["team", "player", "total_points", "singles_weekly_ppd", "total_stars", "total_perfects"]
+    list_filter = [
+        "match__week__season",
+        "match__week__division",
+        "match__week__week_number",
+    ]
+    list_display = [
+        "player",
+        "team",
+        "singles_weekly_ppd",
+        "total_points",
+        "total_stars",
+        "total_perfects",
+    ]
+    search_fields = [
+        "player",
+        "team__name",
+    ]
+    ordering = [
+        "-match__week__season",
+        "match__week__week_number",
+        "player__first_name",
+        "player__last_name",
+    ]
 
 
 @admin.register(ScoreDetail)
 class ScoreDetailAdmin(admin.ModelAdmin):
-    list_filter = ["match__week__division", "match__week__week_number"]
-    list_display = ["team", "player"]
+    list_filter = [
+        "match__week__season",
+        "match__week__division",
+        "match__week__week_number",
+    ]
+    list_display = [
+        "player",
+        "team",
+    ]
 
 
 @admin.register(TeamScoreSummary)
 class TeamScoreSummaryAdmin(admin.ModelAdmin):
-    list_filter = ["match__week__division", "match__week__week_number"]
-    list_display = ["team", "weekly_ppd"]
+    list_filter = [
+        "match__week__season",
+        "match__week__division",
+        "match__week__week_number",
+    ]
+    list_display = [
+        "team",
+        "weekly_ppd",
+    ]
