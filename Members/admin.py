2d1f1a3cbbb120d8060e696f476227aa07285866
b-isfor-basic <overt_link0m@icloud.com>
2024-02-01 23:43:27 -0500

    fixes team admin search function

--- Members/admin.py
+++ Members/admin.py
@@ -21,7 +21,7 @@ class PlayerAdmin(UserAdmin, admin.ModelAdmin):
     ]
     fieldsets += UserAdmin.fieldsets[2:]
     search_fields = ["first_name", "last_name", "username", "email", "phoneNumber"]
-    
+
 
 @admin.register(Registration)
 class RegistrationAdmin(admin.ModelAdmin):
@@ -43,11 +43,9 @@ class TeamAdmin(admin.ModelAdmin):
     list_filter = ["season", ("division", admin.RelatedOnlyFieldListFilter)]
     filter_horizontal = ["players"]
     inlines = [RegistrationInline]
-    search_fields = ["team__players__last_name"]
+    search_fields = ["players__last_name"]
 
     @admin.display(description="Name")
     def name(self, obj):
         plyrs = obj.players.all()
         return plyrs[0].last_name + "/" + plyrs[1].last_name
-    
-    
