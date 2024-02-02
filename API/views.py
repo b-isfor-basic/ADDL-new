73a02a87075cdf0a6284893987a175f921788a56
b-isfor-basic <overt_link0m@icloud.com>
2024-02-01 23:08:34 -0500

    add player form to api view

--- API/views.py
+++ API/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import requires_csrf_token
 
+from rest_framework import generics
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from Members.models import Player, Team
@@ -36,17 +38,29 @@ def create_sub(request):
 @requires_csrf_token
 def sub_list(request, season):
     if request.method == "GET":
-        objects = Player.objects.all().prefetch_related('scoresummary_set', 'scoresummary_set__match', 'scoresummary_set__match__week')
-        objects = objects.filter(scoresummary__match__week__season=season).filter(scoresummary__is_sub=True)
+        objects = Player.objects.all().prefetch_related(
+            "scoresummary_set",
+            "scoresummary_set__match",
+            "scoresummary_set__match__week",
+        )
+        objects = objects.filter(scoresummary__match__week__season=season).filter(
+            scoresummary__is_sub=True
+        )
         serializer = SubstituteSerializer(objects, many=True)
         return Response(serializer.data)
-    
+
 
 @api_view(["GET"])
 @login_required
 @requires_csrf_token
 def team_list(request):
     if request.method == "GET":
-        objects = Team.objects.all().prefetch_related('players')
+        objects = Team.objects.all().prefetch_related("players")
         serializer = TeamSerializer(objects, many=True)
         return Response(serializer.data)
+
+
+class PlayerList(generics.ListCreateAPIView):
+    queryset = Player.objects.all()
+    serializer_class = SubstituteSerializer
+    permission_classes = [IsAuthenticated]
